a6cfbb60c12311a292b798885ae4e6f0ecaaf39f
ovidiomanteiga@gmail.com <ovidiomanteiga@gmail.com>
2013-09-17 04:08:49 +0200

    Simple test case to show up Assertion Error in C5 HashedLinkedList

---
+++ C5HashedLinkedListAssertionError/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace C5HashedLinkedListAssertionError
+{
+    using System;
+
+    using C5;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new HashedLinkedList<int>();
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                list.Add(i);
+
+                if (i % 1000 == 0)
+                {
+                    Console.WriteLine("List size = " + i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
